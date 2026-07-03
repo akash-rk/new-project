7a878a2411fa411fcf719516f9b2ad86207068d4
akash kamble <193405624+akash-rk@users.noreply.github.com>
2026-07-03 17:44:40 +0530

    Refactor neobank.py with function stubs
    Added function placeholders and improved balance display logic.

--- neobank.py
+++ neobank.py
@@ -4,21 +4,19 @@ account_database = {
     "balance": 25000
 }
 
-# --- YOUR FUNCTIONS GO HERE ---
+
 
 def show_balance():
-     print(account_database)# Write logic to print the name and balance
+     print(account_database)
 
 def deposit(amount):
-    # Write logic to add money to the dictionary balance
+    
     pass
 
 def withdraw(amount):
-    # Write logic to deduct money safely if funds exist
+    
     pass
 
-
-# --- THE MAIN SERVER LOOP ---
 while True:
     print("\n=== WELCOME TO NEOBANK CORE ===")
     print("1. Check Balance")
@@ -31,5 +29,4 @@ while True:
         print(f"check Balance {account_database['balance']}")
     elif choice == 2:
         print(f"Deposite money {account_database}")
-    # Write your if/elif/else logic here to call the functions
-    # Hint: If choice is "2", ask for the deposit amount first, then call deposit(amount)
+    
